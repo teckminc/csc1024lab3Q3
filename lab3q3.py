5aaf275834f47c7557c3d8c94dcf17522bfb3065
teckminc <102782040+teckminc@users.noreply.github.com>
2024-02-08 17:26:23 +0800

    Update lab3q3.py

--- lab3q3.py
+++ lab3q3.py
@@ -2,9 +2,9 @@
 3.	Write a Python program using the while loop for calculation that 
 prompts the user to enter two numbers 'a' and 'b' and calculate 'a' to the power of 'b'.
 
-Function to uses: print()
-Operator: +=, <
-Structure: while 
+Function to uses: print(), str(), input(), int()
+Operator: +=, <=
+Structure: while
 '''
 def main():
 
